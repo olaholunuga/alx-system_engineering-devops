22bd45c1c14535093b8de3bf9ff90e87a5028ba0
“olaholunuga” <olaholunuga@gmail.com>
2022-03-13 23:29:40 +0100

    last

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,4 @@
 # using puppet to make changes to an SSH config file
-exec { 'echo':
-  path    => 'usr/bin:/bin',
-  command => 'echo "    IdentityFile ~/.ssh/holberton\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
-  returns => [0,1],
+exec { 'echo "    IdentityFile ~/.ssh/holberton\n    PasswordAuthentication no" >> /etc/ssh/ssh_config':
+  path    => '/bin/'
 }
