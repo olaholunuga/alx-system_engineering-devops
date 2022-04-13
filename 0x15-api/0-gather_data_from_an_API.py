37f5cadc24b81ad67dd49ae40e305adf802eece9
“olaholunuga” <olaholunuga@gmail.com>
2022-04-13 22:07:42 +0100

    ad

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
 """
-Python script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress.
+Python script that, using this RE/her TODO list progress.
 """
 
 import requests
 import sys
 
 API = "https://jsonplaceholder.typicode.com"
+"""API URL"""
+
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         user_id = int(sys.argv[1])
-        user_request = requests.get("{}/users/{}".format(API, str(user_id))).json()
-        user_todos = requests.get("{}/todos".format(API)).json()
-        todos = list(filter(lambda x: x.get("userId") == user_id, user_todos))
+        user_request = requests.get(
+                "{}/users/{}".format(API, str(user_id))).json()
+        user_todos = requests.get(
+                "{}/todos".format(API)).json()
+        todos = list(filter(lambda x: x.get("userId") == user_id,
+            user_todos))
         todos_done = list(filter(lambda x: x.get("completed"), todos))
         user_name = user_request.get("name")
         print(
